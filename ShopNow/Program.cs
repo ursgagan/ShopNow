6970eb838b9d63c3b79c750a718697f02908d1b5
Gagandeep Singh <deepsinghh46@gmail.com>
2024-05-01 12:55:42 +0530

    Updates of 01-05-2024

---
+++ ShopNow/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using ShopNow.BAL.Services;
+using ShopNow.DAL.Data;
+using ShopNow.DAL.Entities;
+using ShopNow.DAL.Interfaces;
+using ShopNow.DAL.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ProductCategoryServices>();
+builder.Services.AddScoped<IRepository<ProductCategory>, ProductCategoryRepository>();
+
+builder.Services.AddScoped<ProductServices>();
+builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
+
+builder.Services.AddScoped<ImageServices>();
+builder.Services.AddScoped<IImageRepository, ImageRepository>();
+
+var connectionString = builder.Configuration.GetConnectionString("ConnStr");
+builder.Services.AddDbContext<ShopNowDbContext>(x =>
+{
+    x.UseSqlServer(connectionString);
+    x.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
